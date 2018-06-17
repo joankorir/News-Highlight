94afb7481bbad1629ced1d023234a1109db4b35d
Your Name <you@example.com>
2018-06-17 20:38:20 +0300

    Added a now_showing variable

--- app/views.py
+++ app/views.py
@@ -13,9 +13,10 @@ def index():
 
     #Getting popular news 
     popular_news = get_news('popular')
-    print(popular_news)
+    upcoming_news = get_news('upcoming')
+    now_showing_news = get_news('now_trending')
     title = 'Home - Welcome to The News Highlight Website Online'
-    return render_template('index.html',title =title,popular=popular_news)
+    return render_template('index.html',title =title,popular=popular_news ,upcoming = upcoming_news , now_showing = now_showing_news
 
 
 
