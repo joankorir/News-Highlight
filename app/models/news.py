0ff3039e00e1ada2d88e717322094ea61aa75711
Your Name <you@example.com>
2018-06-17 09:10:17 +0300

    under the __init__ added four parameters

--- app/models/news.py
+++ app/models/news.py
@@ -3,7 +3,7 @@ class News:
     News class to define news highlight Objects
     '''
 
-    def __init__(self,id,title,overview,image):
+    def  __init__(self,id,title,overview,image):
         self.id =id
         self.title = title
         self.overview = overview
