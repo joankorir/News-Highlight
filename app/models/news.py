b80d458c9fd03008010b4a2abee8274e1c8d6fbe
Your Name <you@example.com>
2018-06-17 09:03:14 +0300

    defined a news class under the news.py

---
+++ app/models/news.py
@@ -0,0 +1,11 @@
+class News:
+    '''
+    News class to define news highlight Objects
+    '''
+
+    def __init__(self,id,title,overview,image):
+        self.id =id
+        self.title = title
+        self.overview = overview
+        self.image = 'https://image.tmdb.org/t/p/w500/'+image
+        
